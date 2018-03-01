881f17f5432737fcc8fd3e3e945ce79588fd7173
Malgorzata Maciejowska <malgorzatamaciejowska21@gmail.com>
2018-03-01 19:34:39 +0100

    stworzono branch dla dzielenia

--- src/OOCalculator/Commands/Add.cs
+++ src/OOCalculator/Commands/Add.cs
@@ -1,12 +1,12 @@
 namespace OOCalculator.Commands
 {
-    public class Add : CommandBase
+    public class Divide : CommandBase
     {
-        public override string Name => "+";
+        public override string Name => "/";
 
         public override int Process(int a, int b)
         {
-            return a + b;
+            return a / b;
         }
     }
 }
--- src/OOCalculator/ManualCommandFactory.cs
+++ src/OOCalculator/ManualCommandFactory.cs
@@ -10,6 +10,7 @@ namespace OOCalculator
         public ManualCommandFactory()
         {
             _commands.Add(new Add());
+            _commands.Add(new Divide());
         }
         public ICommand GetCommand(string name)
         {
